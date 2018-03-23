e585e6a193ea0c52e0bf799da160efdbb7711fd2
bqevin <kevin.barasa001@gmail.com>
2018-03-23 21:47:12 +0300

    Tips text randomizer set

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -12,64 +12,79 @@ import {
 } from 'react-native-elements';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tips : [
+        "The best things in life aren't things!", 
+        "Most of the things your worry about in life never happens",
+        "The future belongs to the few of us willing to get our hands dirty",
+        "Work hard and be nice to people",
+        "Those we meet change us forever",
+        "Be happy for this moment. This moment is your life.",
+        "Shutdown computer. Go outside meet someone",
+        "I want to spend all my life getting out of debt with you"
+      ],
+      tipText: undefined
+    };
+  }
+  componentWillMount(){
+     let tipText;
+     tipText = this.state.tips[Math.floor(Math.random()*this.state.tips.length)];
+    this.setState({ tipText });
+  
+  }
     render() {
-        return(
-            <View>
-                <Header
-                    statusBarProps={{ barStyle: 'light-content',backgroundColor: "#4F6D7A" }}
-                    outerContainerStyles={{ backgroundColor: '#30396c' }}
-                    leftComponent={{ icon: 'menu', color: '#fff'}}
-                    centerComponent={{ text: 'LENDING ASSISTANT', style: { color: '#76b3dc',fontWeight:'600',fontSize: 15,fontFamily: 'proxima-nova-regular' } }}
-                />
-                <Tile
-                    style={{paddingTop: '0', paddingBottom: '0'}}
-                    imageSrc={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRinc8uAMmTqS4J4aVvifVMtNlIXxest8WB0o-Wwe2q-s7bxwC04Q'}}
-                    title="Hi Kevin, Good morning"
-                    featured
-                    caption="Some Caption Text"
-                />
-                <Card>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('LoanStatus')}>
-                        <Text style={styles.card}>
-                            Check my Status
-                        </Text>
-                    </TouchableOpacity>
-                </Card>
+        return <View>
+            <Header 
+              statusBarProps={{ barStyle: "light-content", backgroundColor: "#4F6D7A" }}
+              outerContainerStyles={{ backgroundColor: "#30396c" }}
+              leftComponent={{ icon: "menu", color: "#fff" }} 
+              centerComponent={{ text: "LENDING ASSISTANT", style: { color: "#76b3dc", fontWeight: "600", fontSize: 15, fontFamily: "proxima-nova-regular" } }} 
+            />
+            <Tile style={{ paddingTop: "0", paddingBottom: "0", height: "180" }} 
+              imageSrc={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRinc8uAMmTqS4J4aVvifVMtNlIXxest8WB0o-Wwe2q-s7bxwC04Q" }} 
+              title="Hi Kevin, Good morning" featured 
+              caption={this.state.tipText} 
+              imageContainerStyle={{ height: 180 }} 
+            />
+            <Card>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate("LoanStatus")}>
+                <Text style={styles.card}>Check my Status</Text>
+                <Text>Check how much you qualify for</Text>
+              </TouchableOpacity>
+            </Card>
 
-                <Card>
-                    <TouchableOpacity  onPress={() => console.log('Apply for a loan')}>
-                        <Text style={styles.card}>
-                            Apply for loan
-                        </Text>
-                    </TouchableOpacity>
-                </Card>
-                <Card>
-                    <TouchableOpacity onPress={() => console.log('Increase my Rating')}>
-                        <Text style={styles.card}>
-                            Increase my Ratings
-                        </Text>
-                    </TouchableOpacity>
-                </Card>
-            </View>
-        );
+            <Card>
+              <TouchableOpacity onPress={() => console.log("Apply for a loan")}>
+                <Text style={styles.card}>Apply for loan</Text>
+                <Text>Get your loan now</Text>
+              </TouchableOpacity>
+            </Card>
+            <Card>
+              <TouchableOpacity onPress={() => console.log("Increase my Rating")}>
+                <Text style={styles.card}>Increase my Ratings</Text>
+                <Text>Improve your credit score &amp; earn points</Text>
+              </TouchableOpacity> 
+            </Card>
+          </View>;
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#30396c',
-    },
-    card: {
-        color:'#8b91a3',
-        fontWeight: '400',
-        justifyContent: 'center',
-        marginBottom: 5,
-        fontFamily:'proxima-nova-regular',
-        alignItems:'center',
-        fontSize: 20
-
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#30396c"
+  },
+  card: {
+    color: "#76b3dc",
+    fontWeight: "400",
+    justifyContent: "center",
+    marginBottom: 5,
+    fontFamily: "proxima-nova-regular",
+    alignItems: "center",
+    fontSize: 20
+  }
 });
--- components/pages/Tabs/Offer.js
+++ components/pages/Tabs/Offer.js
@@ -40,80 +40,71 @@ export default class Offer extends React.Component {
         const buttons = [{ element: component1 }, { element: component2 }, { element: component3 }];
         const loans = ['1000', '2000', '3000'];
         const {  selectedAmt, selectedTerm } = this.state;
-        return (
-            <View>
-                <View style={{flex: 1, alignItems: 'stretch', justifyContent: 'center', marginTop:80}}>
-                    <Slider
-                        style={{marginLeft: 20, paddingRight: 20}}
-                        minimumValue={this.state.minValue}
-                        maximumValue={this.state.maxValue}
-                        step={this.state.minValue}
-                        value={this.state.value}
-                        minimumTrackTintColor = '#80cb84'
-                        maximumTrackTinkColor = '#80cb84'
-                        thumbTintColor = '#80cb84'
-                        onValueChange={(value) => this.setState({value})} />
-                    <Text style={{fontFamily:'proxima-nova-light',color:'#80cb84',textAlign: 'center', marginBottom: 70, fontSize: 25, fontWeight: '400'}}>
-                        Current Amount: {this.state.value}
-                        </Text>
-                </View>
-                <Card>
-                    <Text style={styles.header}>
-                        AMOUNT
-                    </Text>
-                    <ButtonGroup
-                        onPress={this.updateAmt}
-                        selectedIndex={selectedAmt}
-                        buttons={loans}
-                        containerStyle={{height: 50}}/>
-                </Card>
-                <Card>
-                    <Text style={styles.header}>
-                        PAYMENT TERMS
-                    </Text>
-                    <ButtonGroup
-                        onPress={this.updateTerm}
-                        selectedIndex={selectedTerm}
-                        buttons={buttons}
-                        containerStyle={{height: 40}} />
-                    <Text style={styles.welcome}>
-                        The quicker you pay, less interest you pay
-                    </Text>
-                </Card>
-
-                <Card>
-                    <View>
-                        <Grid style={{paddingTop: 10, paddingBottom:10}}>
-                            <Col style={{ height: 200, borderEndWidth:1,alignItems: 'center' }}>
-                                <Text>Amount</Text>
-                                <Text>1000</Text>
-                            </Col>
-                            <Col style={{ height: 200, borderEndWidth:1,alignItems: 'center' }}>
-                                <Text>Amount</Text>
-                                <Text>50</Text>
-                            </Col>
-                            <Col style={{ height: 200, alignItems: 'center' }}>
-                                <Text>Balance</Text>
-                                <Text>1050</Text>
-                            </Col>
-                        </Grid>
-                    </View>
-                    <Text style={{textAlign: 'center', margin: 10, color: '#333333', fontSize: 18}}>
-                    </Text>
-                </Card>
-                <Card>
-                    <Text style={{textAlign: 'center',  color: '#333333', fontSize: 18, fontWeight:'bold'}}>
-                        Due Date: 01/03/2018
-                    </Text>
-                </Card>
-                <Button
-                    icon={{name: 'send'}}
-                    backgroundColor='#80cb84'
-                    fontFamily='proxima-nova-regular'
-                    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                    title='SEND LOAN' />
+        return <View>
+            <View style={{ flex: 1, alignItems: "stretch", justifyContent: "center", marginTop: 80 }}>
+              <Slider style={{ marginLeft: 20, paddingRight: 20 }} minimumValue={this.state.minValue} maximumValue={this.state.maxValue} step={this.state.minValue} value={this.state.value} minimumTrackTintColor="#80cb84" maximumTrackTinkColor="#80cb84" thumbTintColor="#80cb84" onValueChange={value => this.setState(
+                    { value }
+                  )} />
+              <Text
+                style={{
+                  fontFamily: "proxima-nova-light",
+                  color: "#80cb84",
+                  textAlign: "center",
+                  marginBottom: 70,
+                  fontSize: 25,
+                  fontWeight: "400"
+                }}
+              >
+                Current Amount: {this.state.value}
+              </Text>
             </View>
-        );
+            <Card>
+              <Text style={styles.header}>
+                Select amounts you want loaned
+              </Text>
+              <ButtonGroup onPress={this.updateAmt} selectedIndex={selectedAmt} buttons={loans} containerStyle={{ height: 50 }} />
+            </Card>
+            <Card>
+              <Text style={styles.header}>PAYMENT TERMS</Text>
+              <ButtonGroup onPress={this.updateTerm} selectedIndex={selectedTerm} buttons={buttons} containerStyle={{ height: 40 }} />
+              <Text style={styles.welcome}>
+                The quicker you pay, less interest you pay
+              </Text>
+            </Card>
+
+            <Card>
+              <View>
+                <Grid style={{ paddingTop: 10, paddingBottom: 10 }}>
+                  <Col style={{ height: 200, borderEndWidth: 1, alignItems: "center" }}>
+                    <Text>Amount</Text>
+                    <Text>1000</Text>
+                  </Col>
+                  <Col style={{ height: 200, borderEndWidth: 1, alignItems: "center" }}>
+                    <Text>Amount</Text>
+                    <Text>50</Text>
+                  </Col>
+                  <Col style={{ height: 200, alignItems: "center" }}>
+                    <Text>Balance</Text>
+                    <Text>1050</Text>
+                  </Col>
+                </Grid>
+              </View>
+              <Text style={{ textAlign: "center", margin: 10, color: "#333333", fontSize: 18 }} />
+            </Card>
+            <Card>
+              <Text
+                style={{
+                  textAlign: "center",
+                  color: "#333333",
+                  fontSize: 18,
+                  fontWeight: "bold"
+                }}
+              >
+                Due Date: 01/03/2018
+              </Text>
+            </Card>
+            <Button icon={{ name: "send" }} backgroundColor="#80cb84" fontFamily="proxima-nova-regular" buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }} title="SEND LOAN" />
+          </View>;
     }
 }
 const styles = StyleSheet.create({
